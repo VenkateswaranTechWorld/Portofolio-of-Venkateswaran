8c8b6978ad954ecb71645b32833fb98ec739c004
Venkateswaran <venkateswaran0906@gmail.com>
2026-04-14 22:02:44 +0530

    fix: refresh dependencies for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  
-  // ✅ ADD THIS BLOCK
   server: {
-    // This tells Vite to run on port 3000 instead of 5173
-    port: 3000, 
+    port: 3000,
   },
-});
-
-// After adding this, you must save the vite.config.js file.
+  build: {
+    outDir: 'dist',
+  }
+})
